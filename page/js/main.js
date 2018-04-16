2ae47d93bf6f2c91e73737876717e642b37e835b
Gustavo Moreira <gustavomn93@gmail.com>
2018-04-15 23:57:39 -0300

    Arrumando funcao do carrossel

--- page/js/main.js
+++ page/js/main.js
@@ -3,9 +3,9 @@ $(function(){
 	$('.testimony img:eq(0)').addClass("ativo").show();
     var texto = $(".ativo").attr("alt");
     $('.testimony').prepend("<p>"+texto+"</p>");
-    setInterval(slide,3000);
+    setInterval(testimony,3000);
 
-    function slide() {
+    function testimony() {
     	if($('.ativo').next().size()) {
     		$('.ativo').fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
     	} else {
